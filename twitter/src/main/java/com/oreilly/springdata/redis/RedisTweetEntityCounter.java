b649ed59e3087fa81889aae0b45213f52a2126ce
mpollack <mpollack@vmware.com>
2012-10-13 22:57:27 -0400

    update processing logic for hastags that are numbers

--- twitter/src/main/java/com/oreilly/springdata/redis/RedisTweetEntityCounter.java
+++ twitter/src/main/java/com/oreilly/springdata/redis/RedisTweetEntityCounter.java
@@ -1,5 +1,8 @@
 package com.oreilly.springdata.redis;
 
+import org.apache.commons.lang.math.NumberUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
@@ -7,6 +10,8 @@ import com.oreilly.springdata.twitter.EntityAwareTweet;
 
 public class RedisTweetEntityCounter implements InitializingBean {
 
+	private static final Log log = LogFactory.getLog(RedisTweetEntityCounter.class);
+	
 	private final StringRedisTemplate redisTemplate;
 
 	public RedisTweetEntityCounter(StringRedisTemplate redisTemplate) {
@@ -19,6 +24,9 @@ public class RedisTweetEntityCounter implements InitializingBean {
 
 	public void count(EntityAwareTweet entityAwareTweet) {
 		for (String hashtag : entityAwareTweet.getTags()) {
+			if (NumberUtils.isNumber(hashtag)) {
+				log.info("hashtag [" + hashtag + " is a number.  tweet = " + entityAwareTweet);
+			}
 			this.redisTemplate.boundZSetOps("hashtags").incrementScore(hashtag, 1.0);
 		}
 		for (String ticker : entityAwareTweet.getTickerSymbols()) {
